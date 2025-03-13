f58442c4430e46cfa515ae13296bb3625866e115
Augustin Godiscal <hello@auggod.tech>
2025-03-13 17:21:55 +0100

    Add missing header for html

--- web/password_reset.go
+++ web/password_reset.go
@@ -99,6 +99,8 @@ func (s *Service) passwordResetForm(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
+	w.Header().Set("Content-Type", "text/html; charset=utf-8")
+
 	PasswordReset(
 		s.cnf.IsDevelopment,
 		r.URL.Path,
